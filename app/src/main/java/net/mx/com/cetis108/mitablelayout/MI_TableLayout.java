a400a45b61603d57c9743530dad3e151e34092bf
Victor Peñuelas <Victor Peuelas>
2017-05-27 11:53:06 -0700

    Initial commit

---
+++ app/src/main/java/net/mx/com/cetis108/mitablelayout/MI_TableLayout.java
@@ -0,0 +1,13 @@
+package net.mx.com.cetis108.mitablelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MI_TableLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi__table_layout);
+    }
+}
